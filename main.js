c4d7b3ce111fadaee511da885202a4b75d4bbfae
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-05-14 00:07:30 -0700

    Changing size of sprites and adding grid.

--- main.js
+++ main.js
@@ -3,15 +3,15 @@
 /*
 5/12 
 Bugs: 1. A player can move more than 2 spaces, after the bug is fix the count>=2 in turnEnd function should be change to ==
-	  2. Case when there's a player in between the path, but still able to pass through it
-	  3. Players on same team shouldn't be allow to move at the same time
-	  4.  functions for detecting nearby players dont work for all cases, ex: isUp() function
+      2. Case when there's a player in between the path, but still able to pass through it
+      3. Players on same team shouldn't be allow to move at the same time
+      4.  functions for detecting nearby players dont work for all cases, ex: isUp() function
 
 */
 
 
 
-var blah_size = 65; // characters' size                                  
+var blah_size = 16; // characters' size                                  
 var red;
 var red1;
 var redX;           // the xy block red in
@@ -39,6 +39,7 @@ var teamBlue = new Array();
 var prompts;
 var redTeam_index = 0;
 var blueTeam_index = 0;
+var battleTheme1;
 
 
 var Game = {};
@@ -57,17 +58,22 @@ Game.main.prototype =
 preload:
     function()
     {
-        this.load.image('background', 'phaser/myGame/assets/black.jpg');
-        this.load.image('red','phaser/myGame/assets/red.png');
-        this.load.image('blue','phaser/myGame/assets/blue.png');
-
+        this.load.image('background', 'phaser/myGame/assets/level1.png');
+        this.load.image('red','phaser/myGame/assets/kaito01.png');
+       // this.load.image('red','phaser/myGame/assets/kenta.png');
+        this.load.image('blue','phaser/myGame/assets/EnemyRed01.png');
+        this.load.audio('battleTheme1', 'phaser/myGame/bgm/Fighting is not an option.mp3');
     },
 
 create:
     function()
     {
-
-        this.add.image(0, 0, 'background');
+        game.scale.setGameSize(960, 960);
+        var background = this.add.image(0, 0, 'background');
+        background.scale.setTo(1.45,1.45);
+        battleTheme1 = game.add.audio('battleTheme1');
+        battleTheme1.loop = true;
+        battleTheme1.play();
 
         //.create 2d array
         for(var i =0; i < 15; i++)
@@ -84,20 +90,20 @@ create:
         //player 1
         red = this.add.sprite(455, 455, 'red');
         red_object =  new Unit('red',10,5,5,0,'redTeam',false,7,7);           // create objs
-		red.width = blah_size;
+        red.width = blah_size;
         red.height = blah_size;
         red.inputEnabled = true;
         game.input.onDown.add(this.selected, {character: red, character_object : red_object, teams: 'red_team', teamArray: teamRed  });
-		
-		//player 2
-		red1 = this.add.sprite(650, 650, 'red');
+        
+        //player 2
+        red1 = this.add.sprite(650, 650, 'red');
         red_object1 =  new Unit('red1',10,6,5,0,'redTeam',false,10,10);           // create objs
-		red1.width = blah_size;
+        red1.width = blah_size;
         red1.height = blah_size;
         red.inputEnabled = true;
         game.input.onDown.add(this.selected, {character: red1, character_object : red_object1, teams: 'red_team', teamArray: teamRed  });
-		
-		
+        
+        
         //players 3
         blue = this.add.sprite(260, 455, 'blue');
         blue_object =  new Unit('blue',10,2,2,0,'blueTeam',true,4,7);
@@ -106,18 +112,18 @@ create:
         blue.inputEnabled = true;
         game.input.onDown.add(this.selected, {character: blue, character_object : blue_object, teams: 'blue_team', teamArray: teamBlue });
         // game.input.onDown.add(this.selected,  {red_sprite: red});
-		
-		
-		//players4
-		blue1 = this.add.sprite(195, 195, 'blue');
+        
+        
+        //players4
+        blue1 = this.add.sprite(195, 195, 'blue');
         blue_object1 =  new Unit('blue1',10,2,2,0,'blueTeam',true,3,3);
         blue1.width = blah_size;
         blue1.height = blah_size;
         blue1.inputEnabled = true;
         game.input.onDown.add(this.selected, {character: blue1, character_object : blue_object1, teams: 'blue_team', teamArray: teamBlue });
-		
-		
-		
+        
+        
+        
         //    5/11
 
         game.physics.arcade.enable(red); // for debug mode, delete after
@@ -126,12 +132,12 @@ create:
         blue.enableBody = true;
         game.physics.arcade.enable(red1);
         red1.enableBody = true;
-		game.physics.arcade.enable(blue1);
+        game.physics.arcade.enable(blue1);
         blue1.enableBody = true;
 
 
 
-        this.add.text(450, 360, 'blue object: ' + myarray[4][7].name, { fontSize: '20px', fill: '#FF0000' });
+        //this.add.text(450, 360, 'blue object: ' + myarray[4][7].name, { fontSize: '20px', fill: '#FF0000' });
 
     },
 
@@ -280,7 +286,7 @@ selected:
 
 
 
-render:
+/*render:
     function()
     {
 
@@ -288,10 +294,10 @@ render:
 
         game.debug.body(red);
         game.debug.body(blue);
-		game.debug.body(red1);
-		game.debug.body(blue1);
+        game.debug.body(red1);
+        game.debug.body(blue1);
 
-    },
+    },*/
 
 };
 
