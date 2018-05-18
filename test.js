4dcb975974cdfcc125c4bf2dcb35e8da13a8e870
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-05-18 00:53:43 -0700

    Adding movement.

--- test.js
+++ test.js
@@ -33,6 +33,7 @@ var gameLevel1 = [
 
 var background;
 var backgroundVir;
+var moveButton;
 var battleTheme1;
 var kaito;
 var virus;
@@ -43,31 +44,84 @@ var playerTeam = new Array();
 var enemyTeam = new Array();
 var level1Width = 30; // Will be used to make copying code for the other levels easier,
 var level1Height = 30; // since it will involve only changing variable names and not hardcoded values.
+var xSelected;
+var ySelected;
 
 var level1Battle = function(game) {};
 level1Battle.prototype = {
 	preload: function() {
         game.load.atlas('atlas', 'phaser/myGame/assets/allSprites.png', 'phaser/myGame/assets/allSprites.json'); // Load the atlas.
 		game.load.image('background', 'phaser/myGame/assets/level1.png');
-		game.load.image('backgroundVir', 'phaser/myGame/assets/Level_1 rouge virtual.png');
+		game.load.image('backgroundVir', 'phaser/myGame/assets/level1v.png');
+		game.load.image('greenTile', 'phaser/myGame/assets/highlight.png');
+		game.load.image('moveButton', 'phaser/myGame/assets/movefullicon.png');
     	game.load.audio('battleTheme1', 'phaser/myGame/bgm/Fighting is not an option.mp3');
 	},
 
 	create: function() {
 		game.scale.setGameSize(960, 530);
-		background = game.add.image(0, 0, 'background');
-		backgroundVir = game.add.image(game.world.width/2, 0, 'backgroundVir');
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		background = game.add.sprite(0, 0, 'background');
+		background.inputEnabled = true;
+		backgroundVir = game.add.sprite(game.world.width/2, 0, 'backgroundVir');
+		//moveButton = game.add.button(0, game.world.height - 50, 'moveButton', chooseSquare, this);
         battleTheme1 = game.add.audio('battleTheme1');
         battleTheme1.loop = true;
         battleTheme1.play();
-        kaitoObject = new Unit('Kaito', 20, 10, 5, 10, 'player', false, 19, 3, 'atlas', 'kaito01');
+        kaitoObject = new Level1Unit('Kaito', 20, 10, 5, 10, 'player', false, 19, 3, 'atlas', 'kaito01');
         game.add.existing(kaitoObject);
-        virusObject = new Unit('Virus', 25, 5, 5, 10, 'enemy', true, 20, 3, 'atlas', 'kenta01');
-        game.add.existing(virusObject);
+        virusObject = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 15, 13, 'atlas', 'kenta01');
+        game.add.existing(virusObject)
+        virusObject1 = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 20, 3, 'atlas', 'kenta01');
+        game.add.existing(virusObject1);
+        virusObject2 = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 7, 24, 'atlas', 'kenta01');
+        game.add.existing(virusObject2);
 	},
 	update: function(){
-
 	}
+}
+
+var chosenSquare = false;
+var newX;
+var newY;
+
+/*function chooseSquare() {
+	chosenSquare = !chosenSquare;
+}*/
+
+function clickSquare() {
+	var newUnit = this;
+	var xCoord = this.xPlace;
+	var yCoord = this.yPlace;
+	if(chosenSquare == true) {
+		xSelected = Math.floor(game.input.mousePointer.x / 16);
+		ySelected = Math.floor(game.input.mousePointer.y / 16);
+		console.log(xSelected);
+		console.log(ySelected);
+		console.log(xCoord);
+		console.log(yCoord);
+		console.log(gameLevel1[xSelected][ySelected] instanceof Level1Unit);
+		if((xSelected >= 0 && xSelected < 30) && (ySelected >= 0 && ySelected < 30)) {
+			console.log("hello");
+			if((gameLevel1[xSelected][ySelected] instanceof Level1Unit) == false) {
+				console.log("yes");
+				gameLevel1[xSelected][ySelected] = newUnit;
+				this.xPlace = xSelected;
+				this.yPlace = ySelected;
+				gameLevel1[xCoord][yCoord] = 0;
+				console.log(gameLevel1[xSelected][ySelected] instanceof Level1Unit);
+				changeSprite(this, this.xPlace, this.yPlace);
+			}
+		}
+	}
+}
+
+function changeSprite(change) {
+	change.pathTiles.visible = false;
+	change.x = change.xPlace * 16;
+	change.y = change.yPlace * 16;
+	change.pathTiles.removeAll();
+	change.pathsFound = false;
 }
 
 function turnEnd (player) {             // if all characters on red team turnEnd == true, set all blue team member   turnEnd == false
