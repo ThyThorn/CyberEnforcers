a0f0a3a24935f46d4d03518a0d2419ffea53e020
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-05-19 20:08:57 -0700

    Adding attack button.

--- level1Battle.js
+++ level1Battle.js
@@ -57,6 +57,7 @@ level1Battle.prototype = {
         game.load.image('backgroundVir', 'phaser/myGame/assets/level1v.png');
         game.load.image('greenTile', 'phaser/myGame/assets/highlight.png');
         game.load.image('moveButton', 'phaser/myGame/assets/movefullicon.png');
+        game.load.image('attackButton', 'phaser/myGame/assets/attackfullicon.png');
         game.load.audio('battleTheme1', 'phaser/myGame/bgm/Fighting is not an option.mp3');
     },
 
@@ -68,20 +69,21 @@ level1Battle.prototype = {
         background.events.onInputDown.add(chooseUnit, this);
         backgroundVir = game.add.sprite(game.world.width/2, 0, 'backgroundVir');
         moveButton = game.add.button(0, game.world.height - 50, 'moveButton', moveUnit, this);
+        attackButton = game.add.button(155, game.world.height - 50, 'attackButton', attackUnit, this);
         battleTheme1 = game.add.audio('battleTheme1');
         battleTheme1.loop = true;
         battleTheme1.play();
-        kaitoObject = new Level1Unit('Kaito', 20, 10, 5, 10, 'player', false, 19, 3, 'atlas', 'kaito01');
+        kaitoObject = new PhysUnit('Kaito', 20, 10, 5, 10, 'player', false, 19, 3, 'atlas', 'kaito01');
         game.add.existing(kaitoObject);
-        kaitoObject1 = new Level1Unit('Kaito1', 20, 10, 5, 10, 'player', false, 14, 10, 'atlas', 'kaito01');
+        kaitoObject1 = new PhysUnit('Kaito1', 20, 10, 5, 10, 'player', false, 14, 10, 'atlas', 'kaito01');
         game.add.existing(kaitoObject1);
-        kaitoObject2 = new Level1Unit('Kaito2', 20, 10, 5, 10, 'player', false, 7, 23, 'atlas', 'kaito01');
+        kaitoObject2 = new PhysUnit('Kaito2', 20, 10, 5, 10, 'player', false, 7, 23, 'atlas', 'kaito01');
         game.add.existing(kaitoObject2);
-        virusObject = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 15, 13, 'atlas', 'kenta01');
+        virusObject = new PhysUnit('Virus', 25, 5, 5, 10, 'enemy', true, 15, 13, 'atlas', 'kenta01');
         game.add.existing(virusObject);
-        virusObject1 = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 20, 3, 'atlas', 'kenta01');
+        virusObject1 = new PhysUnit('Virus', 25, 5, 5, 10, 'enemy', true, 20, 3, 'atlas', 'kenta01');
         game.add.existing(virusObject1);
-        virusObject2 = new Level1Unit('Virus', 25, 5, 5, 10, 'enemy', true, 7, 24, 'atlas', 'kenta01');
+        virusObject2 = new PhysUnit('Virus', 25, 5, 5, 10, 'enemy', true, 7, 24, 'atlas', 'kenta01');
         game.add.existing(virusObject2);
     },
     update: function(){
@@ -90,6 +92,7 @@ level1Battle.prototype = {
 
 var chosenSquare = false;
 var movePressed = false;
+var attackPressed = false;
 var chosenX;
 var chosenY;
 
@@ -102,34 +105,57 @@ function setInvisible() {
     }
 }
 
+function disableButtons() {
+    moveButton.input.enabled = false;
+    attackButton.input.enabled = false;
+}
+
+function enableButtons() {
+    moveButton.input.enabled = true;
+    attackButton.input.enabled = true;
+}
+
 function moveUnit() {
     setInvisible();
     movePressed = true;
-    moveButton.input.enabled = false;
+    attackPressed = false;
+    disableButtons();
     console.log(movePressed);
 }
 
+function attackUnit() {
+    setInvisible();
+    attackPressed = true;
+    movePressed = false;
+    disableButtons();
+    console.log('attackPressed: ' + attackPressed);
+}
+
 var newUnit;
+var attacker;
 
 function chooseUnit() {
     notEnemy:
     if(chosenSquare == false) {
         chosenX = Math.floor(game.input.mousePointer.x / 16);
         chosenY = Math.floor(game.input.mousePointer.y / 16);
-        console.log('hello');
-        if(gameLevel1[chosenX][chosenY] instanceof Level1Unit) {
+        if(gameLevel1[chosenX][chosenY] instanceof PhysUnit) {
             setInvisible();
             gameLevel1[chosenX][chosenY].pathFinder();
             if(turnNumber % 2 == 1) {
                 if(gameLevel1[chosenX][chosenY].team == 'player') {
                     if(movePressed == true) {
                         chosenSquare = true;
-                        movePressed = false;
                         newUnit = gameLevel1[chosenX][chosenY];
+                        return;
+                    }
+                    if(attackPressed == true) {
+                        setInvisible();
+                        chosenSquare = true;
+                        attacker = gameLevel1[chosenX][chosenY];
                     }
                 }
                 else {
-                    console.log('No, you cannot do that.');
                     break notEnemy;
                 }
             }
@@ -137,72 +163,76 @@ function chooseUnit() {
                 if(gameLevel1[chosenX][chosenY].team == 'enemy') {
                     if(movePressed == true) {
                         chosenSquare = true;
-                        movePressed = false;
                         newUnit = gameLevel1[chosenX][chosenY];
+                        return;
+                    }
+                    if(attackPressed == true) {
+                        setInvisible();
+                        chosenSquare = true;
+                        attacker = gameLevel1[chosenX][chosenY];
                     }
                 }
                 else {
-                    console.log('No, you cannot do that.');
                     break notEnemy;
                 }
             }
         } 
         else {
             setInvisible();
-            moveButton.input.enabled = true;
+            enableButtons();
         }
     }
-    else {
-        xSelected = Math.floor(game.input.mousePointer.x / 16);
-        ySelected = Math.floor(game.input.mousePointer.y / 16);
-        console.log('hey');
-        console.log('chosenX: ' + chosenX);
-        console.log('chosenY: ' + chosenY);
-        console.log('xSelected: ' + xSelected);
-        console.log('ySelected: ' + ySelected);
-        if((xSelected >= 0 && xSelected < level1Width) && (ySelected >= 0 && ySelected < level1Height)) {
-            if((gameLevel1[xSelected][ySelected] instanceof Level1Unit) == false) {
-                console.log('yay');
-                if((chosenX > xSelected && game.math.difference(chosenX, xSelected) <= newUnit.leftMax && chosenY == ySelected))
-                {
-                    console.log('holla');
-                    newUnit.movesDone += game.math.difference(chosenX, xSelected);
-                    gameLevel1[xSelected][ySelected] = newUnit;
-                    newUnit.xPlace = xSelected;
-                    newUnit.yPlace = ySelected;
-                    gameLevel1[chosenX][chosenY] = 0;
-                    changeSprite(newUnit);
-                }
-                else if((xSelected > chosenX && game.math.difference(chosenX, xSelected) <= newUnit.rightMax && chosenY == ySelected)) {
-                    console.log('yellow');
-                    newUnit.movesDone += game.math.difference(chosenX, xSelected);
-                    gameLevel1[xSelected][ySelected] = newUnit;
-                    newUnit.xPlace = xSelected;
-                    newUnit.yPlace = ySelected;
-                    gameLevel1[chosenX][chosenY] = 0;
-                    changeSprite(newUnit);
-                }
-                else if((chosenY > ySelected && game.math.difference(chosenY, ySelected) <= newUnit.upMax && chosenX == xSelected)) {
-                    console.log('wright');
-                    newUnit.movesDone += game.math.difference(chosenY, ySelected);
-                    gameLevel1[xSelected][ySelected] = newUnit;
-                    newUnit.xPlace = xSelected;
-                    newUnit.yPlace = ySelected;
-                    gameLevel1[chosenX][chosenY] = 0;
-                    changeSprite(newUnit);
-                }
-                else if((ySelected > chosenY && game.math.difference(chosenY, ySelected) <= newUnit.downMax && chosenX == xSelected)) {
-                    console.log('wit');
-                    newUnit.movesDone += game.math.difference(chosenY, ySelected);
-                    gameLevel1[xSelected][ySelected] = newUnit;
-                    newUnit.xPlace = xSelected;
-                    newUnit.yPlace = ySelected;
-                    gameLevel1[chosenX][chosenY] = 0;
-                    changeSprite(newUnit);
-                }
-                movePressed = false;
-                chosenSquare = false;
+    else if(movePressed == true) {
+        chooseSquare();
+    }
+    else if(attackPressed == true) {
+        attacker.checkEnemy();
+    }
+}
+
+function chooseSquare() {
+    xSelected = Math.floor(game.input.mousePointer.x / 16);
+    ySelected = Math.floor(game.input.mousePointer.y / 16);
+    if((xSelected >= 0 && xSelected < level1Width) && (ySelected >= 0 && ySelected < level1Height)) {
+        if((gameLevel1[xSelected][ySelected] instanceof PhysUnit) == false) {
+            if((chosenX > xSelected && game.math.difference(chosenX, xSelected) <= newUnit.leftMax && chosenY == ySelected))
+            {
+                newUnit.movesDone += game.math.difference(chosenX, xSelected);
+                gameLevel1[xSelected][ySelected] = newUnit;
+                newUnit.xPlace = xSelected;
+                newUnit.yPlace = ySelected;
+                gameLevel1[chosenX][chosenY] = 0;
+                changeSprite(newUnit);
             }
+            else if((xSelected > chosenX && game.math.difference(chosenX, xSelected) <= newUnit.rightMax && chosenY == ySelected)) {
+                newUnit.movesDone += game.math.difference(chosenX, xSelected);
+                gameLevel1[xSelected][ySelected] = newUnit;
+                newUnit.xPlace = xSelected;
+                newUnit.yPlace = ySelected;
+                gameLevel1[chosenX][chosenY] = 0;
+                changeSprite(newUnit);
+            }
+            else if((chosenY > ySelected && game.math.difference(chosenY, ySelected) <= newUnit.upMax && chosenX == xSelected)) {
+                newUnit.movesDone += game.math.difference(chosenY, ySelected);
+                gameLevel1[xSelected][ySelected] = newUnit;
+                newUnit.xPlace = xSelected;
+                newUnit.yPlace = ySelected;
+                gameLevel1[chosenX][chosenY] = 0;
+                changeSprite(newUnit);
+            }
+            else if((ySelected > chosenY && game.math.difference(chosenY, ySelected) <= newUnit.downMax && chosenX == xSelected)) {
+                newUnit.movesDone += game.math.difference(chosenY, ySelected);
+                gameLevel1[xSelected][ySelected] = newUnit;
+                newUnit.xPlace = xSelected;
+                newUnit.yPlace = ySelected;
+                gameLevel1[chosenX][chosenY] = 0;
+                changeSprite(newUnit);
+            }
+            movePressed = false;
+            chosenSquare = false;
+            setInvisible();
+            enableButtons();
+            return;
         }
     }
 }
@@ -222,8 +252,8 @@ function changeSprite(change) {
     change.downMax = 0;
     chosenSquare = false;
     movePressed = false;
-    moveButton.input.enabled = true;
     moveUnit();
+    enableButtons();
     turnEnd(change.team);
 }
 
