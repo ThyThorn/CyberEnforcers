97771a36a33221712aa7dcf15e69175afd587107
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-05-22 02:53:20 -0700

    Adding a special effect to the title screen.

--- title.js
+++ title.js
@@ -3,21 +3,28 @@ var titleMusic;
 var title = function(game) {};
 title.prototype = {
     preload: function() {
-        game.load.image('sky', 'phaser/myGame/assets/sky.png');
-        game.load.audio('title', 'phaser/myGame/assets/S31-Hit and Run.mp3'); // Load title theme.
+        game.load.image('park', 'assets/img/park.jpg');
+        game.load.image('logo', 'assets/img/titlescreen.png');
+        game.load.audio('title', 'assets/audio/S31-Hit and Run.mp3'); // Load title theme.
     },
     create: function() {
-        game.add.sprite(0, 0, 'sky');
-        game.add.text(140,160, 'Cyber Enforcers', { fontSize: '40px', fill: '#000' });
-        game.add.text(170,250, 'Press Enter to start playing.', { fontSize: '16px', fill: '#000' });
+        var park = game.add.sprite(0, 0, 'park');
+        park.scale.setTo(0.5, 0.5);
+        game.add.sprite(220, 100, 'logo');
+        game.add.text(340,350, 'Press Enter to start playing.', { fontSize: '16px', fill: '#000' });
         titleMusic = game.add.audio('title');
         titleMusic.loop = true; // Let title theme loop.
         titleMusic.play(); // Start playing the theme.
     },
     update: function() {
-        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) { // Press the Enter key to begin gameplay.
+        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) { // Press the Enter key to begin the next scene.
             game.sound.stopAll(); // All sounds from title state should not be in the next state.
-            game.state.start("beginningDialog");
+            game.camera.fade(0x000000); // Make the screen fade to black.
+            game.camera.onFadeComplete.add(this.beginScene1, this); // Once the screen fades to black, do this function.
         }
+    },
+
+    beginScene1: function() {
+        game.state.start("beginningDialog"); // Go to the next state.
     }
 }
