781c91581d090756b9c5df04dcd799173769ae44
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-05-25 11:59:41 -0700

    Updating node effects a little.

--- node.js
+++ node.js
@@ -14,12 +14,23 @@ function Node(named,effect,north,south,west,east,xPlace,yPlace,unit){
 
 Node.prototype.constructor = Node;
 
+var timer;
+
 Node.prototype.whichKind = function() {
+	background.inputEnabled = false;
 	if(this.effect == 'green') {
-		this.greenNode();
+		this.checkAOE();
+		timer = game.time.create(false);
+		timer.add(1000, Node.prototype.greenNode, this);
+		timer.add(4000, Node.prototype.destroy, this);
+		timer.start();
 	}
 	else if(this.effect == 'red') {
-		this.redNode();
+		this.checkAOE();
+		timer = game.time.create(false);
+		timer.add(1000, Node.prototype.redNode, this);
+		timer.add(4000, Node.prototype.destroy, this);
+		timer.start();
 	}
 	else if(this.effect == 'blue') {
 		blueNodeVar = true;
@@ -33,36 +44,38 @@ Node.prototype.checkAOE = function() {
 		}
 	}
 	northBound -= 1;
-	console.log(northBound);
 	for(southBound = 1; southBound <= yAOE; southBound++) {
 		if(this.yPlace + southBound >= levelHeight) {
 			break;
 		}
 	}
 	southBound -= 1;
-	console.log(southBound);
 	for(westBound = 1; westBound <= xAOE; westBound++) {
 		if(this.xPlace - westBound < 0) {
 			break;
 		}
 	}
 	westBound -= 1;
-	console.log(westBound);
 	for(eastBound = 1; eastBound <= xAOE; eastBound++) {
 		if(this.xPlace + eastBound >= levelWidth) {
 			break;
 		}
 	}
 	eastBound -= 1;
-	console.log(eastBound);
+}
+
+Node.prototype.destroy = function() {
+	this.AOETiles.removeAll();
+	hackNodeButton.loadTexture('hackNodeButton');
+	enableButtons();
+	background.inputEnabled = true;
 }
 
 Node.prototype.greenNode = function() {
-	this.checkAOE();
 	for(var i = this.yPlace - northBound; i <= this.yPlace; i++) {
 		for(var j = this.xPlace - westBound; j <= this.xPlace; j++) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health += 5;
 				} 
@@ -70,7 +83,7 @@ Node.prototype.greenNode = function() {
 		}
 		for(var j = this.xPlace + eastBound; j > this.xPlace; j--) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health += 5;
 				}
@@ -80,7 +93,7 @@ Node.prototype.greenNode = function() {
 	for(var i = this.yPlace + southBound; i > this.yPlace; i--) {
 		for(var j = this.xPlace - westBound; j <= this.xPlace; j++) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health += 5;
 				} 
@@ -88,31 +101,36 @@ Node.prototype.greenNode = function() {
 		}
 		for(var j = this.xPlace + eastBound; j > this.xPlace; j--) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health += 5;
 				} 
 			}
 		}
 	}
-}
+},
 
 Node.prototype.redNode = function() {
-	this.checkAOE();
 	for(var i = this.yPlace - northBound; i <= this.yPlace; i++) {
 		for(var j = this.xPlace - westBound; j <= this.xPlace; j++) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health -= 10;
+					if(gameLevel[j][i].health <= 0) {
+						gameLevel[j][i].deleteFromTeam();
+					}
 				} 
 			}
 		}
 		for(var j = this.xPlace + eastBound; j > this.xPlace; j--) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health -= 10;
+					if(gameLevel[j][i].health <= 0) {
+						gameLevel[j][i].deleteFromTeam();
+					}
 				}
 			}
 		}
@@ -120,17 +138,23 @@ Node.prototype.redNode = function() {
 	for(var i = this.yPlace + southBound; i > this.yPlace; i--) {
 		for(var j = this.xPlace - westBound; j <= this.xPlace; j++) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
-					gameLevel[j][i].health -= 10;
+					gameLevel[j][i].health -= 10;			
+					if(gameLevel[j][i].health <= 0) {
+						gameLevel[j][i].deleteFromTeam();
+					}	
 				} 
 			}
 		}
 		for(var j = this.xPlace + eastBound; j > this.xPlace; j--) {
 			if(gameLevel[j][i] != 1) {
-				blueTile = this.AOETiles.create(j * 16, i * 16, 'greenTile');
+				blueTile = this.AOETiles.create(j * 16 + shiftPhysFactor, i * 16 + shiftPhysFactor, 'greenTile');
 				if(gameLevel[j][i] instanceof PhysUnit) {
 					gameLevel[j][i].health -= 10;
+					if(gameLevel[j][i].health <= 0) {
+						gameLevel[j][i].deleteFromTeam();
+					}
 				} 
 			}
 		}
