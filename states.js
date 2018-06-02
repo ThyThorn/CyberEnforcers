b04ccac2486e69f391ea3c101b652c945d501003
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-06-02 00:53:51 -0700

    Update states.js

--- states.js
+++ states.js
@@ -1,12 +1,5 @@
-// In your index.html, it should have the following:
-/* <script type= "text/javascript" src = "phaser/myGame/js/character.js"> </script>
-<script type= "text/javascript" src = "phaser/myGame/js/title.js"> </script>
-<script type= "text/javascript" src = "phaser/myGame/js/beginningDialog.js"> </script>
-<script type= "text/javascript" src = "phaser/myGame/js/main.js"> </script>
-<script type= "text/javascript" src = "phaser/myGame/js/states.js"> </script>*/
-
-var game = new Phaser.Game(1200, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(960, 600, Phaser.AUTO, '');
 game.state.add("title", title);
-game.state.add("beginningDialog", beginningDialogPlay);
-game.state.add("battle", Game.main);
+game.state.add("level1Dialog", beginningDialogPlay);
+game.state.add("level1Battle", level1Battle);
 game.state.start("title");
