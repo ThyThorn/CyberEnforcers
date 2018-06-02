bdb194b867aee6549cdb7579f9bbd5b3bcdc800d
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-06-02 00:49:21 -0700

    Saving title code.

--- title.js
+++ title.js
@@ -12,6 +12,7 @@ title.prototype = {
         park.scale.setTo(2, 2);
         game.add.sprite(220, 100, 'logo');
         game.add.text(340,350, 'Press Enter to start playing.', { fontSize: '16px', fill: '#000' });
+        game.add.text(340, 390, 'Press UP to go to first battle.', { fontSize: '16px', fill: '#000' });
         titleMusic = game.add.audio('title');
         titleMusic.loop = true; // Let title theme loop.
         titleMusic.play(); // Start playing the theme.
@@ -22,13 +23,18 @@ title.prototype = {
             game.camera.fade(0x000000); // Make the screen fade to black.
             game.camera.onFadeComplete.add(this.beginScene1, this); // Once the screen fades to black, do this function.
         }
-        if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) { // Press the Enter key to begin the next scene.
-            game.sound.stopAll();
-            game.state.start('level1Battle');
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            game.sound.stopAll(); // All sounds from title state should not be in the next state.
+            game.camera.fade(0x000000); // Make the screen fade to black.
+            game.camera.onFadeComplete.add(this.battle1, this); // Once the screen fades to black, do this function.
         }
     },
 
     beginScene1: function() {
-        game.state.start("beginningDialog"); // Go to the next state.
+        game.state.start("level1Dialog"); // Go to the next state.
+    },
+
+    battle1: function() {
+        game.state.start("level1Battle");
     }
 }
