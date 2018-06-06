8d13706e9ff0c79dbdb56ca49a28678abe08545e
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-06-05 22:00:13 -0700

    Update title.js

--- title.js
+++ title.js
@@ -1,40 +1,62 @@
 var titleMusic;
+var title;
+var startButton;
+var creditsButton;
 
 var title = function(game) {};
 title.prototype = {
     preload: function() {
         game.load.image('sky', 'assets/img/sky.png');
         game.load.image('logo', 'assets/img/titlescreen.png');
+        game.load.image('startButton', 'assets/img/startfullicon.png');
+        game.load.image('creditsButton', 'assets/img/creditsfullicon.png');
         game.load.audio('title', 'assets/audio/S31-Hit and Run.mp3'); // Load title theme.
+        game.load.audio('confirmation', 'assets/audio/Confirmation sound.wav');
     },
     create: function() {
-        var park = game.add.sprite(0, 0, 'sky');
-        park.scale.setTo(2, 2);
+        title = game.add.sprite(0, 0, 'sky');
+        title.scale.setTo(2, 2);
+        confirmation = game.add.audio('confirmation');
         game.add.sprite(220, 100, 'logo');
-        game.add.text(340,350, 'Press Enter to start playing.', { fontSize: '16px', fill: '#000' });
-        game.add.text(340, 390, 'Press UP to go to first battle.', { fontSize: '16px', fill: '#000' });
+        startButton = game.add.button(400, 350, 'startButton', startGame);
+        creditsButton = game.add.button(400, 410, 'creditsButton', startCredits);
         titleMusic = game.add.audio('title');
         titleMusic.loop = true; // Let title theme loop.
         titleMusic.play(); // Start playing the theme.
     },
+
     update: function() {
-        if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) { // Press the Enter key to begin the next scene.
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) { // This will be deleted later.
             game.sound.stopAll(); // All sounds from title state should not be in the next state.
-            game.camera.fade(0x000000); // Make the screen fade to black.
-            game.camera.onFadeComplete.add(this.beginScene1, this); // Once the screen fades to black, do this function.
-        }
-        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            game.sound.stopAll(); // All sounds from title state should not be in the next state.
+            confirmation.play();
             game.camera.fade(0x000000); // Make the screen fade to black.
             game.camera.onFadeComplete.add(this.battle1, this); // Once the screen fades to black, do this function.
         }
     },
 
-    beginScene1: function() {
-        game.state.start("level1Dialog"); // Go to the next state.
-    },
-
     battle1: function() {
-        game.state.start("level1Battle");
-    }
+        game.state.start("gameOver");
+    },
+}
+
+startGame = function() {
+    game.sound.stopAll(); // All sounds from title state should not be in the next state.
+    confirmation.play();
+    game.camera.fade(0x000000, 2500); // Make the screen fade to black.
+    game.camera.onFadeComplete.add(beginScene1, this); // Once the screen fades to black, do this function.
+}
+
+beginScene1 = function() {
+    game.state.start("prolog"); // Go to the next state.
+},
+
+startCredits = function() {
+    game.sound.stopAll(); // All sounds from title state should not be in the next state.
+    confirmation.play();
+    game.camera.fade(0x000000, 2500); // Make the screen fade to black.
+    game.camera.onFadeComplete.add(goToCredits, this); // Once the screen fades to black, do this function.
+}
+
+goToCredits = function() {
+    game.state.start("credits");        
 }
