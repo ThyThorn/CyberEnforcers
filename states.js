19736d5f31784bef2ffdd4632779841777b3cbd1
ZhaoLi45255 <37007785+ZhaoLi45255@users.noreply.github.com>
2018-06-05 21:58:41 -0700

    Update states.js

--- states.js
+++ states.js
@@ -1,5 +1,15 @@
 var game = new Phaser.Game(960, 600, Phaser.AUTO, '');
 game.state.add("title", title);
-game.state.add("level1Dialog", beginningDialogPlay);
+game.state.add("credits", credits);
+game.state.add("prolog", prolog);
+game.state.add("level1Dialog", level1Dialog);
 game.state.add("level1Battle", level1Battle);
+game.state.add("level2Dialog", level2Dialog);
+game.state.add("level2Battle", level2Battle);
+game.state.add("level3Dialog", level3Dialog);
+game.state.add("level3Battle", level3Battle);
+game.state.add("level4Dialog", level4Dialog);
+game.state.add("level4Battle", level4Battle);
+game.state.add("ending", ending);
+game.state.add("gameOver", gameOver);
 game.state.start("title");
